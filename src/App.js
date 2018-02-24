da943b2e3749169bc5684f150740a4d718b00013
Matt Comben <matthew@dockstudios.co.uk>
2018-02-24 14:33:12 +0000

    Fix issues with showing/hidding

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'semantic-ui-css/semantic.min.css';
-import { Card, Icon, Image, Menu, MenuItem, Modal, Header, Segment, Grid } from 'semantic-ui-react'
+import { Card, Icon, Image, Menu, MenuItem, Modal, Header, Segment, Grid, Sidebar, Button, Checkbox } from 'semantic-ui-react'
 
 class ImageModal extends Component {
   getImageUrl = () => {
-    return this.props.getBaseApiUrl() + '/photo/' + this.props.media_id + '/data';
+    return this.props.getBaseApiUrl() + '/media/' + this.props.media_id + '/data';
   };
   render() {
     if (this.props.media_id) {
@@ -80,7 +80,7 @@ class EditIcon extends PhotoIcon {
 
 class HideIcon extends PhotoIcon {
   getIconName = () => {
-    if (this.props.active) {
+    if (this.props.active == 2) {
       return 'hide';
     } else {
       return 'unhide'
@@ -105,7 +105,7 @@ class Photo extends Component {
 
   state = {
     backup_state: 0,
-    hide_state: false,
+    hidden_state: false,
     media_count: 0,
     name: ''
   };
@@ -140,14 +140,20 @@ class Photo extends Component {
     //   selected_year = this.props.selected_year;
     //   selected_month = this.props.selected_month;
     // }
-    fetch(this.getBaseItemUrl(id, type, selected_year, selected_month))
+    let get_url = this.getBaseItemUrl(id, type, selected_year, selected_month);
+    if (this.props.show_hidden) {
+      get_url += '?show_hidden=1';
+    }
+    fetch(get_url)
       .then(res => res.json())
       .then(res => {
         this.setState((previousState) => ({
           name: res.name,
           backup_state: res.backup_state,
-          media_count: res.media_count
+          media_count: res.media_count,
+          hidden_state: res.hidden_state
         }));
+        console.log(res.hidden_state);
       });
   };
 
@@ -157,6 +163,15 @@ class Photo extends Component {
     })
     .then(() => {
       this.getObjectDetails(this.props.id, this.props.type, this.props.selected_year, this.props.selected_month);
+    });
+  }
+
+  toggleHide = () => {
+    fetch(this.getBaseItemUrl(this.props.id, this.props.type, this.props.selected_year, this.props.selected_month) + '/hide', {
+      method: 'POST'
+    })
+    .then(() => {
+      this.props.updateObjectIds();
     });
   }
 
@@ -177,20 +192,6 @@ class Photo extends Component {
     return media_count_text;
   };
 
-  toggleHide = () => {
-    this.setState((previousState) => ({
-      hide_state: !(previousState.hide_state)
-    }));
-  };
-
-  getHideIcon = () => {
-    if (this.state.hide_state) {
-      return 'unhide'
-    } else {
-      return 'hide'
-    }
-  };
-
   onClick = () => {
     this.props.updateSelected(this.props.type, this.props.id)
   }
@@ -207,7 +208,7 @@ class Photo extends Component {
           <Card.Content extra>
             <EditIcon onClick={this.editName} />
             <BackupIcon onClick={this.toggleBackup} active={this.state.backup_state} />
-            <HideIcon onClick={this.setHide} active={this.state.hide_state} />
+            <HideIcon onClick={this.toggleHide} active={this.state.hidden_state} />
             <ExpandIcon onClick={this.expand} />
           </Card.Content>
         </Card>
@@ -226,7 +227,9 @@ class PhotoViewer extends Component {
                                                   type={this.props.shown_type}
                                                   id={obj_id}
                                                   selected_year={this.props.selected_year}
-                                                  selected_month={this.props.selected_month} />)}
+                                                  selected_month={this.props.selected_month}
+                                                  updateObjectIds={this.props.updateObjectIds}
+                                                  show_hidden={this.props.show_hidden} />)}
       </Grid>
     );
   };
@@ -239,16 +242,12 @@ class Breadcrumb extends Component {
   }
   render() {
     if (this.props.selected_name) {
-      if (this.props.type === 'year') {
-        return (<a className="section" onClick={this.set_filter}>{this.props.selected_name}</a>);
-      } else {
-        return (
-          <span>
-            <div className="divider"> / </div>
-            <a className="section" onClick={this.set_filter}>{this.props.selected_name}</a>
-          </span>
-        );
-      }
+      return (
+        <span>
+          <div className="divider"> / </div>
+          <a className="section" onClick={this.set_filter}>{this.props.selected_name}</a>
+        </span>
+      );
     } else {
       return (null);
     }
@@ -264,7 +263,8 @@ class TopMenu extends Component {
       <Menu>
       <MenuItem>
       <div className="ui breadcrumb">
-        <a onClick={this.clear_filters} className="section"> / </a>
+        <Button onClick={this.props.toggleSideBar}>Show Filters</Button>
+        <a onClick={this.clear_filters} className="selection">Root</a>
         <Breadcrumb type='year' updateSelected={this.props.updateSelected} selected_name={this.props.selected_year} selected_value={this.props.selected_year} />
         <Breadcrumb type='month' updateSelected={this.props.updateSelected} selected_name={this.props.selected_month} selected_value={this.props.selected_month} />
         <Breadcrumb type='day' updateSelected={this.props.updateSelected} selected_name={this.props.selected_day} selected_value={this.props.selected_day} />
@@ -288,7 +288,9 @@ class App extends Component {
     selected_set: null,
     selected_media: null,
     shown_objects: [],
-    shown_type: 'year'
+    shown_type: 'year',
+    sidebar_visible: false,
+    show_hidden: false
   };
 
   updateSelected = (type, id) => {
@@ -336,6 +338,9 @@ class App extends Component {
     } else if (this.state.shown_type == 'media') {
       get_url = '/sets/' + this.state.selected_set + '/media';
     }
+    if (this.state.show_hidden) {
+      get_url += '?show_hidden=1'
+    }
     console.log(get_url);
     fetch(this.getBaseApiUrl() + get_url)
       .then(res => res.json())
@@ -351,8 +356,13 @@ class App extends Component {
     this.updateObjectIds();
   };
 
-  getBaseApiUrl = () => {
-    return 'http://localhost:5000'
+  getBaseApiUrl = () => { return 'http://localhost:5000' };
+
+  toggleSideBar = () => this.setState({ sidebar_visible: !this.state.sidebar_visible });
+  toggleShowHidden = () => {
+    this.setState({ show_hidden: !this.state.show_hidden }, () => {
+      this.updateObjectIds();
+    });
   };
 
   render() {
@@ -363,15 +373,29 @@ class App extends Component {
           selected_month={this.state.selected_month}
           selected_day={this.state.selected_day}
           selected_set={this.state.selected_set}
-          getBaseApiUrl={this.getBaseApiUrl} />
-        <PhotoViewer updateSelected={this.updateSelected}
-          selected_year={this.state.selected_year}
-          selected_month={this.state.selected_month}
-          selected_day={this.state.selected_day}
-          selected_set={this.state.selected_set}
-          shown_objects={this.state.shown_objects}
-          shown_type={this.state.shown_type}
-          getBaseApiUrl={this.getBaseApiUrl} />
+          getBaseApiUrl={this.getBaseApiUrl}
+          toggleSideBar={this.toggleSideBar} />
+
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='overlay' style={{'background-color': 'white'}} width='thin' visible={this.state.sidebar_visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Checkbox onChange={this.toggleShowHidden} value={this.state.show_hidden} label='Show hidden' />
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <PhotoViewer updateSelected={this.updateSelected}
+              updateObjectIds={this.updateObjectIds}
+              selected_year={this.state.selected_year}
+              selected_month={this.state.selected_month}
+              selected_day={this.state.selected_day}
+              selected_set={this.state.selected_set}
+              shown_objects={this.state.shown_objects}
+              shown_type={this.state.shown_type}
+              getBaseApiUrl={this.getBaseApiUrl}
+              show_hidden={this.state.show_hidden} />
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+
         <ImageModal media_id={this.state.selected_media} getBaseApiUrl={this.getBaseApiUrl} />
       </div>
     );
